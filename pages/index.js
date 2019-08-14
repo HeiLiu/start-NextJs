1e1b1314a95835b89de4ab719c5c6be5d8af7ca9
heiliu <969403750@qq.com>
2019-08-14 16:21:06 +0800

    feat: 引入antd 处理css文件

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,27 @@
-import React, {Fragment} from 'react';
+import React, { Fragment } from 'react';
 import Link from 'next/link';
+import Layout from '../components/Layout';
+
+import { Button } from 'antd';
 
 const Home = () => (
-  <Fragment>
-    <h1>Next, 轻量级React 服务端渲染框架</h1>
-    <Link href='userList'>
-      <a>用户列表页</a>
-    </Link>
-  </Fragment>
+  <Layout>
+    <Fragment>
+      <h1>Next, 轻量级React 服务端渲染框架</h1>
+      <Link href='userList'>
+        <Button type="primary">用户列表页</Button>
+      </Link>
+    </Fragment>
+    <style jsx global>{
+    `
+    // 没有生效
+    .antd-btn-primary {
+      background-color: red;
+    }
+    `
+  }</style>
+  </Layout>
+  
 )
 
 export default Home;
